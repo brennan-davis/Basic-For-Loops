c07053e26d4f17f101b1ae92cb6befa503cc58cf
Brennan <brennan.davis@outlook.com>
2022-06-29 23:34:16 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿bool continueLoop = true;
+while(continueLoop)
+{
+    int sum = 0;
+
+    Console.WriteLine("Enter a whole number greater than 1:");
+
+    bool inputIsInt = int.TryParse(Console.ReadLine(), out int num);
+
+    while (!inputIsInt) inputIsInt = int.TryParse(Console.ReadLine(), out num);
+
+    for (int i = 0; i <= num; i++) sum += i;
+
+    Console.WriteLine(sum);
+
+    Console.WriteLine("\nWould you like to continue? (y)");
+    continueLoop = Console.ReadKey().Key == ConsoleKey.Y;
+    Console.WriteLine("\n");
+}
